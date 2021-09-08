c3b95b7b27abea9a51cedf8b6a83bc1c6726db65
soads <90018981+soads@users.noreply.github.com>
2021-09-08 11:23:59 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-logo.png') {
-        myImage.setAttribute ('src','images/logo.png');
+        myImage.setAttribute ('src','images/firefox-icon.png');
     }   else {
         myImage.setAttribute ('src','images/firefox-logo.png');
     }
